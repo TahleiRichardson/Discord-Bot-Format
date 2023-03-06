bf1b28359a86cd5121cd411ebec5ec86d6bf0e81
Tahlei <79224558+TahleiRichardson@users.noreply.github.com>
2023-03-05 20:26:02 -0500

    Update responses.py

---
+++ responses.py
@@ -0,0 +1,20 @@
+import random
+import datetime
+
+def response(msg) ->str:
+
+    #These can be Cphanged or Deleted
+
+    p_msg = msg.lower()
+
+    if p_msg == '!hello':
+        return 'Hello There!'
+
+    elif p_msg == '!roll':
+        return str(random.randint(1,6))
+    
+    elif p_msg == '!help':
+        return 'This is a message that can be changed in the future.'
+    
+    #Add Further Responses Here
+    
